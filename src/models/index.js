e256478c6b300eb312151c6bb6276c3cc0e8277e
fernandocto.scz <fernandocto.scz@gmail.com>
2021-05-23 02:21:40 -0400

    📦 NEW: added dotenv config

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,15 @@
 'use strict'
 
+require('dotenv').config()
+
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
-const env = process.env.NODE_ENV || 'development'
-const config = require(path.join(__dirname, '../../config/config.json'))[env]
-const db = {}
 
-let sequelize
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config)
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config)
-}
+const databaseUrl = process.env.DATABASE_URL
+const db = {}
+const sequelize = new Sequelize(databaseUrl)
 
 fs
     .readdirSync(__dirname)
