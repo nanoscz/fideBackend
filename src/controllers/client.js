e9713212575e434c841f0277c0a91f175e4ab5ca
fernandocto.scz <fernandocto.scz@gmail.com>
2021-06-17 16:18:16 -0400

    👌 IMPROVE: order DESC (client)

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -3,15 +3,21 @@
 const Client = require('../models').client
 
 class ClientController {
-  findAll (req, res, next) {
-    Client.findAll()
+  findAll(req, res, next) {
+
+    const where = {};
+    const order = [
+      ['createAt', 'DESC']
+    ]
+
+    Client.findAll({ where, order })
       .then(clients => {
         res.json(clients)
       })
       .catch(err => next(err))
   }
 
-  findOne (req, res, next) {
+  findOne(req, res, next) {
     Client.findOne({ where: { id: req.params.id } })
       .then(client => {
         res.json(client)
@@ -19,21 +25,21 @@ class ClientController {
       .catch(err => next(err))
   }
 
-  create (req, res, next) {
+  create(req, res, next) {
     const body = req.body
     Client.create(body)
       .then(() => res.status(201).end())
       .catch(err => next(err))
   }
 
-  update (req, res, next) {
+  update(req, res, next) {
     const body = req.body
     Client.update(body, { where: { id: req.params.id } })
       .then(() => res.status(200).end())
       .catch(err => next(err))
   }
 
-  delete (req, res, next) {
+  delete(req, res, next) {
     Client.destroy({ where: { id: req.params.id } })
       .then(() => res.status(204).end())
       .catch(err => next(err))
