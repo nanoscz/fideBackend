ff650905ca7df471008b6578cec9343cee72e0af
fernandocto.scz <fernandocto.scz@gmail.com>
2021-08-07 14:41:07 -0400

    📦 NEW: add migration

---
+++ migrations/20210803232633-product-add-status.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @status 0=disable, 1=enable, 2=all
+ */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("productos", "status", {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn("productos", "status");
+  }
+};
+
